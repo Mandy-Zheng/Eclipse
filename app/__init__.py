6575dcebfefd5948fa18d3d873ca3f3c1e885809
Amanda Zheng <azheng00@stuy.edu>
2020-04-29 18:21:59 -0400

    check boxes on querying data

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,72 @@
 # P04 :: Let the Data Speak
 # 2020-04-??
 
-from flask import Flask
+from flask import Flask , render_template,request, redirect, url_for, session, flash
+from functools import wraps
+import sqlite3, os, random
+import urllib3, json, urllib
 
 app = Flask(__name__)
+abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands':'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+}
+
+countries=["Spain", "China", "Italy", "Iran", "Canada", "South Korea", "Turkey", "United Kingdom", "Egypt", "Zimbabwe", "United States"]
 
 @app.route('/')
 def home():
@@ -13,8 +76,9 @@ def home():
 
 @app.route('/query')
 def query():
-    return render_template("query.html", title="Covid-19 Tracker")
-    
+    states=abbrev.keys()
+    return render_template("query.html", title="Covid-19 Tracker",states=states,countries=countries,state_dict=abbrev)
+
 @app.route('/data')
 def displayData():
     pass
