f85e9d20871a2778e548768373d961f48162f3e8
Tiffany Cao <tcao00@stuy.edu>
2020-04-29 18:25:16 -0400

    laid out html pages

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,18 @@
 # P04 :: Let the Data Speak
 # 2020-04-??
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("_base.html", title="Covid-19 Tracker",heading="Welcome to Covid-19 Tracker!")
+    return render_template("welcome.html", title="Covid-19 Tracker", heading="Welcome to Covid-19 Tracker!")
 
 @app.route('/query')
 def query():
-    return render_template("query.html", title="Covid-19 Tracker")
-    
+    return render_template("query.html", title="Data Selection", heading = "Data Selection")
+
 @app.route('/data')
 def displayData():
     pass
