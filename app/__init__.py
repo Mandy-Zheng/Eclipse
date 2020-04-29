ff4e7ad34292ea1ab689363ce1c3c08801933b2a
azheng00 <azheng00@stuy.edu>
2020-04-28 21:56:49 -0400

    updated templates

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template("_base.html", title="Covid-19 Tracker",heading="Welcome to Covid-19 Tracker!")
 
-
+@app.route('/query')
+def query():
+    return render_template("query.html", title="Covid-19 Tracker")
+@app.route('/data')
+def displayData():
+    pass
 if __name__ == '__main__':
     app.debug = True
     app.run()
