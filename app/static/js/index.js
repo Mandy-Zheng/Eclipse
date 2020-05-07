9a3e5d04b4f5aa384adf24f4d31cb5efaae88584
YifanWang0 <ywang00@stuy.edu>
2020-05-07 13:49:39 -0400

    new branch for pie chart

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,3 +1,4 @@
+/*
 // var data =  [{"country":"Canada","cases":7,"deaths":10},
 // {"country":"China","cases":7,"deaths": 15},{"country":"France","cases":5,"deaths":20}]
 
@@ -245,54 +246,69 @@ const render = function(){
 // var dateBtn = document.getElementById("dateBtn");
 // dateBtn.addEventListener("click", displayDate);
 // dateBtn.addEventListener("click", document.getElementById("dateSelected").innerHTML = "hi");
+*/
 
-/*      .selectAll("text");
+// set the dimensions and margins of the graph
+var width = 450
+    height = 450
+    margin = 40
 
-
-//pie chart code
-
-var pwidth = 250
-var pheight = 250
-var margin= 40
-
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 var radius = Math.min(width, height) / 2 - margin
+
+// append the svg object to the div called 'my_dataviz'
 var svg = d3.select("#pieID")
   .append("svg")
-  .attr("width", width)
-  .attr("height", height)
+    .attr("width", width)
+    .attr("height", height)
   .append("g")
-  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+// create 2 data_set
+var data = {death: 9, recoveries: 20, newCases:30}
+var data1 =  [{"country":"Canada", "cases":7, "deaths":10, "recoveries":9},
+{"country":"China","cases":7,"deaths": 15},{"country":"France","cases":5,"deaths":20}]
 
 // set the color scale
 var color = d3.scaleOrdinal()
-  .domain([0, d3.max(data, function(d) { return d.cases+1; })])
+  .domain(data1)
   .range(d3.schemeDark2);
 
-var pie = d3.pie()
-      .value(function(d) {return d.value; })
-      .sort(function(a, b) { console.log(a) ; return d3.ascending(a.key, b.key);} ) // This make sure that group order remains the same in the pie chart
-var data_ready = pie(d3.entries(data))
-
-    // map to data
-var u = svg.selectAll("path")
-      .data(data_ready)
-
-    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    u
-      .enter()
-      .append('path')
-      .merge(u)
-      .transition()
-      .duration(1000)
-      .attr('d', d3.arc()
-        .innerRadius(0)
-        .outerRadius(radius)
-      )
-      .attr('fill', function(d){ return(color(d.cases)) })
-      .attr("stroke", "white")
-      .style("stroke-width", "2px")
-      .style("opacity", 1)
-
 // A function that create / update the plot for a given variable:
-=======*/
+function update(data) {
+
+  // Compute the position of each group on the pie:
+  var pie = d3.pie()
+    .value(function(d) {return d.value; })
+    .sort(function(a, b) { console.log(a) ; return d3.ascending(a.key, b.key);} ) // This make sure that group order remains the same in the pie chart
+  var data_ready = pie(d3.entries(data))
+
+  // map to data
+  var u = svg.selectAll("path")
+    .data(data_ready)
+
+  // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+  u
+    .enter()
+    .append('path')
+    .merge(u)
+    .transition()
+    .duration(1000)
+    .attr('d', d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius)
+    )
+    .attr('fill', function(d){ return(color(d.data.key)) })
+    .attr("stroke", "white")
+    .style("stroke-width", "2px")
+    .style("opacity", 1)
+
+  // remove the group that is not present anymore
+  u
+    .exit()
+    .remove()
+
+}
+
+// Initialize the plot with the first dataset
+update(data1[0])
--- app/static/js/retrieve_data.js
+++ app/static/js/retrieve_data.js
@@ -1,62 +1,62 @@
-var data;
-//var daysElapsed = document.getElementById("dateSlider").value;
-
-const getQuery = function() {
-    var raw = window.location.search.substring(1);
-    var URLVars = raw.split('&');
-    var v;
-    for (v in URLVars) {
-        var param = URLVars[v].split('=');
-        if (param[0] == 'q') {
-            return param[1];
-        }
-    }
-};
-
-const formatDate = function(datestr) {
-    components = datestr.split('-');
-    var months = {
-        '01':'January',
-        '02':'February',
-        '03':'March',
-        '04':'April',
-        '05':'May',
-        '06':'June',
-        '07':'July',
-        '08':'August',
-        '09':'September',
-        '10':'October',
-        '11':'November',
-        '12':'December'
-    };
-    return `${months[components[1]]} ${components[2]}, ${components[0]}`
-}
-
-const slider = document.getElementById('dateSlider');
-
-const getData = function(daysElapsed) {
-    //daysElapsed should be from 2020-01-21
-    var query = getQuery();
-    $.ajax({
-        method: 'POST',
-        url: '/data',
-        data: {'q': query, 'date': daysElapsed},
-        success: function(retrieved) {
-            var date = document.getElementById('dateSelected');
-            date.innerHTML = formatDate(retrieved['date']);
-            data = retrieved['data'][0];
-            render();
-        }
-    });
-};
-
-const update = function() {
-    getData(slider.value);
-}
-
-slider.addEventListener('input', update);
-
-slider.value = 1;
+// var data;
+// //var daysElapsed = document.getElementById("dateSlider").value;
+//
+// const getQuery = function() {
+//     var raw = window.location.search.substring(1);
+//     var URLVars = raw.split('&');
+//     var v;
+//     for (v in URLVars) {
+//         var param = URLVars[v].split('=');
+//         if (param[0] == 'q') {
+//             return param[1];
+//         }
+//     }
+// };
+//
+// const formatDate = function(datestr) {
+//     components = datestr.split('-');
+//     var months = {
+//         '01':'January',
+//         '02':'February',
+//         '03':'March',
+//         '04':'April',
+//         '05':'May',
+//         '06':'June',
+//         '07':'July',
+//         '08':'August',
+//         '09':'September',
+//         '10':'October',
+//         '11':'November',
+//         '12':'December'
+//     };
+//     return `${months[components[1]]} ${components[2]}, ${components[0]}`
+// }
+//
+// const slider = document.getElementById('dateSlider');
+//
+// const getData = function(daysElapsed) {
+//     //daysElapsed should be from 2020-01-21
+//     var query = getQuery();
+//     $.ajax({
+//         method: 'POST',
+//         url: '/data',
+//         data: {'q': query, 'date': daysElapsed},
+//         success: function(retrieved) {
+//             var date = document.getElementById('dateSelected');
+//             date.innerHTML = formatDate(retrieved['date']);
+//             data = retrieved['data'][0];
+//             render();
+//         }
+//     });
+// };
+//
+// const update = function() {
+//     getData(slider.value);
+// }
+//
+// slider.addEventListener('input', update);
+//
+// slider.value = 1;
 
 // var displayDate = function(daysElapsed) {
 //   console.log(daysElapsed);
