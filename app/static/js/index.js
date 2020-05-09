f99fa9cbc6a5c57d35921690ea68dc57827f82b8
Amanda Zheng <azheng00@stuy.edu>
2020-05-08 20:27:36 -0400

    buttons work

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,37 +2,38 @@ var data =  [{"country":"Canada","cases":7,"deaths":10},
 {"country":"China","cases":7,"deaths": 15},{"country":"France","cases":5,"deaths":20}]
 
 //defining the margin amounts of the chart
-var margin = {top:50, right:50, bottom:50, left:50};
-//the total width of the bar graph
-var height = 3*200-100;
-//the total height of the bar graph
-var width = 800-100;
 
-
-//sets the number of pixels for the yscale
-//adds padding
-var yScale = d3.scaleBand()
-    .range([0, height])
-    .paddingInner(0.08)
-
-//sets the number of pixels for the xscale
-var xScale = d3.scaleLinear()
-    .range([0,width-50]);
-
-//positions the x axis on the top
-var xAxis = d3.axisTop(xScale)
-//positions the y axis on the left
-var yAxis = d3.axisLeft(yScale);
-
-
-//makes a chart with width and height adjusted with margins
-var svgContainer = d3.select("#chartID").append("svg")
-    .attr("width", width+100)
-    .attr("height",height+100)
-    .append("g").attr("class", "container")
-    .attr("transform", "translate("+ 50 +","+ 50 +")");
 
 var initialBar1 = function(d){
+  var margin = {top:50, right:50, bottom:50, left:50};
+  //the total width of the bar graph
+  var height = 3*200-100;
+  //the total height of the bar graph
+  var width = 800-100;
+
+
+  //sets the number of pixels for the yscale
+  //adds padding
+  var yScale = d3.scaleBand()
+      .range([0, height])
+      .paddingInner(0.08)
+
+  //sets the number of pixels for the xscale
+  var xScale = d3.scaleLinear()
+      .range([0,width-50]);
+
+  //positions the x axis on the top
+  var xAxis = d3.axisTop(xScale)
+  //positions the y axis on the left
+  var yAxis = d3.axisLeft(yScale);
+
+
+  //makes a chart with width and height adjusted with margins
+  var svgContainer = d3.select("#chartID").append("svg")
+      .attr("width", width+100)
+      .attr("height",height+100)
+      .append("g").attr("class", "container")
+      .attr("transform", "translate("+ 50 +","+ 50 +")");
   yScale.domain(data.map(function(d) { return d.country; }));
   yAxis = d3.axisLeft(yScale);
 
@@ -100,6 +101,35 @@ var initialBar1 = function(d){
 //for each bar, maps the labels of y scale based on d.cases and d.deaths
 
 var initialBar2 = function(d){
+  var margin = {top:50, right:50, bottom:50, left:50};
+  //the total width of the bar graph
+  var height = 3*200-100;
+  //the total height of the bar graph
+  var width = 800-100;
+
+
+  //sets the number of pixels for the yscale
+  //adds padding
+  var yScale = d3.scaleBand()
+      .range([0, height])
+      .paddingInner(0.08)
+
+  //sets the number of pixels for the xscale
+  var xScale = d3.scaleLinear()
+      .range([0,width-50]);
+
+  //positions the x axis on the top
+  var xAxis = d3.axisTop(xScale)
+  //positions the y axis on the left
+  var yAxis = d3.axisLeft(yScale);
+
+
+  //makes a chart with width and height adjusted with margins
+  var svgContainer = d3.select("#chartID").append("svg")
+      .attr("width", width+100)
+      .attr("height",height+100)
+      .append("g").attr("class", "container")
+      .attr("transform", "translate("+ 50 +","+ 50 +")");
   // height = 3*200-100;
   max1 = d3.max(data, function(d) { return d.cases; });
   max2 = d3.max(data, function(d) { return d.deaths; });
@@ -201,6 +231,131 @@ var initialBar2 = function(d){
 
 }
 
+var initialPie = function(data){
+  for (var i = 0; i < data.length; i++) {
+  // set the dimensions and margins of the graph
+  var width = 450
+      height = 450
+      margin = 50
+
+  // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+  var radius = Math.min(width, height) / 2 - margin
+
+  // append the svg object to the div called 'my_dataviz'
+  var svg = d3.select("#pieID")
+    .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  // set the color scale
+  var color = d3.scaleOrdinal()
+    .domain(data[i])
+    .range(d3.schemeDark2);
+
+  // A function that create / update the plot for a given variable:
+  //
+
+    // Compute the position of each group on the pie:
+    var pie = d3.pie()
+      .value(function(d) {return d.value; })
+      .sort(function(a, b) { console.log(a) ; return d3.ascending(a.key, b.key);} ) // This make sure that group order remains the same in the pie chart
+    var data_ready = pie(d3.entries(data[i]))
+
+    var label = d3.arc()
+              .outerRadius(radius+60)
+              .innerRadius(radius);
+
+    // map to data
+    var u = svg.selectAll("path")
+      .data(data_ready)
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    u
+      .enter()
+      .append('path')
+      .merge(u)
+      .transition()
+      .duration(1000)
+      .attr('d', d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius)
+      )
+      .attr('fill', function(d){return(color(d.data.key)) })
+      .attr("stroke", "white")
+      .style("stroke-width", "2px")
+      .style("opacity", 1)
+
+    svg.selectAll(".text")
+      .data(data_ready)
+      .enter()
+        .append("text")                                     //add a label to each slice
+        .attr("transform", function(d) {                //set the label's origin to the center of the arc
+              //we have to make sure to set these before calling arc.centroid
+          return "translate(" +label.centroid(d)+ ")";        //this gives us a pair of coordinates like [50, 50]
+        })
+        .attr("text-anchor", "middle")                          //center the text on it's origin
+        .text(function(d) {
+          if(!Number.isNaN(d.value)){
+    			  return d.data.key;
+          }
+        })
+
+    // remove the group that is not present anymore
+    u
+      .exit()
+      .remove()
+    }
+  }
+var clearChart = function(){
+  d3.selectAll("svg").remove();
+}
+var newGraph = function(){
+  var r = document.getElementById("r").checked;
+  var d =document.getElementById("d").checked;
+  var n =document.getElementById("n").checked;
+  console.log(r);
+  clearChart();
+  var data =  [{"country":"Canada", "cases":7, "deaths":10, "recoveries":9},
+{"country":"China","cases":7,"deaths": 15},{"country":"France","cases":5,"deaths":20}]
+  if(r && d && n ){
+    initialPie(data);
+  }else if( r && d){
+    var subData=[]
+    for (var i = 0; i < data.length; i++) {
+     var dict={}
+     dict.countries=data[i].countries;
+     dict.recoveries=data[i].recoveries;
+     dict.deaths=data[i].deaths;
+     subData.push(dict);
+    }
+    initialBar2(subData);
+  }else if( r && n){
+    var subData=[]
+    for (var i = 0; i < data.length; i++) {
+     var dict={}
+     dict.countries=data[i].countries;
+     dict.cases=data[i].cases;
+     dict.recoveries=data[i].recoveries;
+     subData.push(dict);
+    }
+    initialBar2(subData);
+  }else if( n && d){
+    var subData=[]
+    for (var i = 0; i < data.length; i++) {
+     var dict={}
+     dict.countries=data[i].countries;
+     dict.cases=data[i].cases;
+     dict.deaths=data[i].deaths;
+     subData.push(dict);
+    }
+    initialBar2(subData);
+  }else if( r || d || n){
+    initialBar1(data);
+  }
+}
+
 // initialBar2(data);
 
 //displaying date
@@ -208,7 +363,7 @@ var daysElapsed = document.getElementById("dateSlider").value;
 
 var displayDate = function(daysElapsed) {
   console.log(daysElapsed);
-
+}
 
 
 
