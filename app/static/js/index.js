710cd10b1aa9bdfa2a966b5da57cefcbdead6f0b
Tiffany Cao <tcao00@stuy.edu>
2020-05-10 00:19:20 -0400

    added comments

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,12 +1,17 @@
+//dummy data
 var num = 0;
 var canadaN = [5, 10, 15, 20, 25, 30, 35, 40];
 var canadaD = [1, 2, 3, 4, 5, 6, 7, 8];
 var canadaR = [10, 20, 30, 40, 50, 60, 70, 80];
-var updateBar1;
 
-//defining the margin amounts of the chart
+//empty vars used for nested function in initialBar1 and initialBar2
+var updateBar1;
+var updateBar2;
+
+//empty var for chart type
 var chartType="";
 
+//function for counting digits in a number
 function digits_count(n) {
   var count = 0;
   if (n >= 1) ++count;
@@ -19,23 +24,25 @@ function digits_count(n) {
   return count;
 };
 
+//function for creating and updating a single-bar bar graph
 var initialBar1 = function(data, l){
   // console.log(data);
+
+  //margins
   var margin = {top:50, right:50, bottom:50, left:50};
   //the total width of the bar graph
-  var height = data.length*200-100;
+  var height = data.length*100-100;
   //the total height of the bar graph
   var width = 1200-100;
 
 
-
-  //sets the number of pixels for the yscale
+  //sets the number of pixels for the y scale
   //adds padding
   var yScale = d3.scaleBand()
       .range([0, height])
       .paddingInner(0.08)
 
-  //sets the number of pixels for the xscale
+  //sets the number of pixels for the x scale
   var xScale = d3.scaleLinear()
       .range([0,width-50]);
 
@@ -52,18 +59,21 @@ var initialBar1 = function(data, l){
       .append("g").attr("class", "container")
       .attr("transform", "translate("+ 100 +","+ 50 +")");
 
+  //maps the countries to the domain of the y scale
   yScale.domain(data.map(function(d) { return d.country; }));
   yAxis = d3.axisLeft(yScale);
 
+  //sets domain of x scale according to the maximum data value + an offset calculated with the number of digits in the maximum
   xScale.domain([0, d3.max(data, function(d) { return parseInt(d.option1) + Math.pow(10, digits_count(parseInt(d.option1)) - 1); })]);
   xAxis = d3.axisTop(xScale);
 
-  //draws the actual bars and does the height based off of data values
+  //selects the bars and enters the data values
   var bars = svgContainer.selectAll(".bar")
       .data(data)
       .enter()
       .append("g")
 
+  //draws the bars
   bars.append("rect")
       .attr("class", "bar")
       .attr("y", function(d) { return yScale(d.country); })
@@ -71,27 +81,26 @@ var initialBar1 = function(data, l){
       .attr("x", function(d) { return 0; })
       .attr("width", function(d) { return xScale(d.option1); });
 
-  //make the numbers on the labels, x value and y value of the numerical labels
+  //selects the labels and enters the data values
   var labeling = svgContainer.selectAll(".text")
       .data(data)
       .enter()
 
+  //creates the labels for the bars
   labeling.append("text")
       .attr("class","label")
-      .attr("y", (function(d) { return yScale(d.country) + yScale.bandwidth()/2; }  ))
-      .attr("x", function(d) { return  xScale(d.option1) + 15; })
+      .attr("y", (function(d) { return yScale(d.country) + 5 + yScale.bandwidth()/2; }  ))
+      .attr("x", function(d) { return  xScale(d.option1) + 12; })
       .attr("dx", ".75em")
       .text(function(d) { return d.option1; });
 
-
-  //creates labels for x scale on the side
+  //creates labels for x scale on the top
   svgContainer.append("g")
       .attr("class", "xaxis")
       .call(xAxis)
       .selectAll("text")
-      // .attr("font-family", "Didot")
 
-  //title for x axis
+  //creates title for x axis
   svgContainer.append("text")
       .attr("x", width/2)
       .attr("y", -35)
@@ -103,9 +112,8 @@ var initialBar1 = function(data, l){
       .attr("class", "yaxis")
       .call(yAxis)
       .selectAll("text")
-      // .attr("font-family", "Didot")
 
-  //title for y axis
+  //creates title for y axis
   svgContainer.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - 100)
@@ -114,12 +122,13 @@ var initialBar1 = function(data, l){
       .style("text-anchor", "middle")
       .text("Country/State");
 
+  //creates the legend and enters the data values
   var legend = d3.select("#chartID").select("svg").selectAll(".legend")
       .data(data)
       .enter().append("g")
       .attr("class", "legend")
-      // .attr("transform", "translate(0,0)");
 
+  //creates the circle for the legend
   legend.append("circle")
       .attr("class", "option1")
       .attr("cx", width + 160)
@@ -127,22 +136,27 @@ var initialBar1 = function(data, l){
       .attr("r", 10)
       .style("fill", "#E5C3D1");
 
+  //creates the labeling for the legend
   legend.append("text")
       .attr("x", width + 60)
       .attr("y", 75)
       .text(""+ l[0] +"");
 
 
+  //function to update the single-bar bar graph
   updateBar1 = function(updatedData){
     // console.log("updating");
     // console.log(updatedData);
 
+    //resets the y scale domain
     yScale.domain(updatedData.map(function(d) { return d.country; }));
     yAxis = d3.axisLeft(yScale);
 
+    //resets the x scale domain with the new maximum data value + the offset calculated with the number of digits in the maximum
     xScale.domain([0, d3.max(updatedData, function(d) { return parseInt(d.option1) + Math.pow(10, digits_count(parseInt(d.option1)) - 1); })]);
     xAxis = d3.axisTop(xScale);
 
+    //resets the labels for the x scale
     svgContainer.select(".xaxis")
            .transition().duration(1000)
            .call(xAxis)
@@ -154,12 +168,11 @@ var initialBar1 = function(data, l){
           .transition()
           .duration(1000)
           .attr("width", function() { testing++; return xScale(updatedData[testing].option1); });
-          // .attr("y", function(d) { return yScale(d.cases); })
     testing = -1;
 
+    //resets the bar number labels and transitions the previous numbers to the new data numbers
     var testing2 = -1;
     var testing3 = -1;
-  //resets the number labels and transitioning the previous numbers to the new data numbers
     svgContainer.selectAll(".label")
         .transition()
         .duration(1000)
@@ -170,30 +183,30 @@ var initialBar1 = function(data, l){
           return function( t ) {
             this.textContent = interpolator( t );
           };})
-        .attr("x", function() { testing3++; return xScale(updatedData[testing3].option1) + 15; })
-        //.attr("x", (function(d) { return xScale(d.country) + xScale.bandwidth() / 2 ; }  ));
+        .attr("x", function() { testing3++; return xScale(updatedData[testing3].option1) + 12; })
     testing2 = -1;
     testing3 = -1;
   }
 
 }
-//for each bar, maps the labels of y scale based on d.cases and d.deaths
 
+//function for creating and updating a double-bar bar graph
 var initialBar2 = function(data, l){
+  //margins
   var margin = {top:50, right:50, bottom:50, left:50};
   //the total width of the bar graph
-  var height = data.length*200-100;
+  var height = data.length*100-100;
   //the total height of the bar graph
   var width = 1200-100;
 
 
-  //sets the number of pixels for the yscale
+  //sets the number of pixels for the y scale
   //adds padding
   var yScale = d3.scaleBand()
       .range([0, height])
       .paddingInner(0.08)
 
-  //sets the number of pixels for the xscale
+  //sets the number of pixels for the x scale
   var xScale = d3.scaleLinear()
       .range([0,width-50]);
 
@@ -209,25 +222,27 @@ var initialBar2 = function(data, l){
       .attr("height",height+100)
       .append("g").attr("class", "container")
       .attr("transform", "translate("+ 100 +","+ 50 +")");
-  // height = 3*200-100;
+
+  //finding the new maximum data value
   max1 = d3.max(data, function(d) { return parseInt(d.option1); });
   max2 = d3.max(data, function(d) { return parseInt(d.option2); });
-  // console.log(max1);
-  // console.log(max2);
   findM = [max1, max2]
-  //sets the scale of the xscale initially
+
+  //sets domain of x scale according to the maximum data value + an offset calculated with the number of digits in the maximum
   xScale.domain([0, parseInt(d3.max(findM)) + Math.pow(10, digits_count(parseInt(d3.max(findM))) - 1) ]);
   xAxis = d3.axisTop(xScale);
 
+  //maps the countries to the domain of the y scale
   yScale.domain(data.map(function(d) { return d.country; }));
   yAxis = d3.axisLeft(yScale);
 
+  //selects the bars and enters the data values
   bars = svgContainer.selectAll(".bar")
       .data(data)
       .enter()
       .append("g");
 
-  //option1
+  //draws the bars for option1 data
   bars.append("rect")
       .attr("class", "bar")
       .attr("y", function(d) { return yScale(d.country); })
@@ -235,7 +250,7 @@ var initialBar2 = function(data, l){
       .attr("x", function(d) { return 0; })
       .attr("width", function(d) { return xScale(d.option1); })
 
-  // option2
+  //draws the bars for option2 data
   bars.append("rect")
       .attr("class", "bar2")
       .attr("y", function(d) { return yScale(d.country); })
@@ -244,25 +259,26 @@ var initialBar2 = function(data, l){
       .attr("x", function(d) { return 0; })
       .attr("transform", function(d) { return "translate(0, "+ yScale.bandwidth()/2 +")"; });
 
+  //selects the labels and enters the data values
   labeling = svgContainer.selectAll(".text")
       .data(data)
       .enter()
 
-  //option1
+  //creates the labels for the option1 bars
   labeling.append("text")
       .attr("class","label")
       .attr("class", "first")
-      .attr("y", (function(d) { return yScale(d.country) + yScale.bandwidth() / 4 ; }  ))
-      .attr("x", function(d) { return  xScale(d.option1) + 15; })
+      .attr("y", (function(d) { return yScale(d.country) + 5 + yScale.bandwidth() / 4 ; }  ))
+      .attr("x", function(d) { return  xScale(d.option1) + 12; })
       .attr("dx", ".75em")
       .text(function(d) { return d.option1; });
 
-  //option2
+  //creates the labels for the option2 bars
   labeling.append("text")
       .attr("class", "label")
       .attr("class", "second")
-      .attr("y", (function(d) { return yScale(d.country) + ((yScale.bandwidth() / 4) * 3) ; } ))
-      .attr("x", function(d) { return xScale(d.option2) + 15; })
+      .attr("y", (function(d) { return yScale(d.country) + 5 + ((yScale.bandwidth() / 4) * 3) ; } ))
+      .attr("x", function(d) { return xScale(d.option2) + 12; })
       .attr("dx", ".75em")
       .text(function(d) {return  d.option2;})
 
@@ -271,9 +287,8 @@ var initialBar2 = function(data, l){
       .attr("class", "xaxis")
       .call(xAxis)
       .selectAll("text")
-      // .attr("font-family", "Didot")
 
-  //title for x axis
+  //creates title for x axis
   svgContainer.append("text")
       .attr("x", width/2)
       .attr("y", -35)
@@ -287,7 +302,7 @@ var initialBar2 = function(data, l){
       .selectAll("text")
       // .attr("font-family", "Didot")
 
-  //title for y axis
+  //creates title for y axis
   svgContainer.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - 100)
@@ -296,12 +311,13 @@ var initialBar2 = function(data, l){
       .style("text-anchor", "middle")
       .text("Country/State");
 
+  //creates the legend and enters the data values
   legend = d3.select("#chartID").select("svg").selectAll(".legend")
       .data(data)
       .enter().append("g")
       .attr("class", "legend")
-      // .attr("transform", "translate(0,0)");
 
+  //creates circle for option1 in the legend
   legend.append("circle")
       .attr("class", "option1")
       .attr("cx", width+160)
@@ -309,11 +325,13 @@ var initialBar2 = function(data, l){
       .attr("r", 10)
       .style("fill", "#E5C3D1");
 
+  //creates the labeling for option1 in the legend
   legend.append("text")
       .attr("x", width + 60)
       .attr("y", 75)
       .text("" + l[0] + "");
 
+  //creates circle for option2 in the legend
   legend.append("circle")
       .attr("class", "option2")
       .attr("cx", width+160)
@@ -321,43 +339,45 @@ var initialBar2 = function(data, l){
       .attr("r", 10)
       .style("fill", "#CAA8F5");
 
+  //creates the labeling for option2 in the legend
   legend.append("text")
       .attr("x", width + 60)
       .attr("y", 105)
       .text("" + l[1] + "");
 
+  //function to update the double-bar bar graph
   updateBar2 = function(updatedData){
     // console.log("here2");
     // console.log(updatedData);
 
+    //resets the y scale domain
     yScale.domain(updatedData.map(function(d) { return d.country; }));
     yAxis = d3.axisLeft(yScale);
 
+    //finding the new maximum data value
     max1 = d3.max(updatedData, function(d) { return parseInt(d.option1); });
     max2 = d3.max(updatedData, function(d) { return parseInt(d.option2); });
-    // console.log(max1);
-    // console.log(max2);
     findM = [max1, max2]
 
+    //resets the x scale domain with the new maximum data value + the offset calculated with the number of digits in the maximum
     xScale.domain([0, parseInt(d3.max(findM)) + Math.pow(10, digits_count(parseInt(d3.max(findM))) - 1) ]);
     xAxis = d3.axisTop(xScale);
 
+    //resets the labels for the x scale
     svgContainer.select(".xaxis")
            .transition().duration(1000)
            .call(xAxis)
            .selectAll("text");
 
-    //resets the lengths of each bar based on new data
+    //resets the lengths of each bar for option1 based on new data
     var testing = -1;
-    // console.log(updatedData[testing].option1);
-    // console.log(updatedData[testing].option2);
-
     svgContainer.selectAll(".bar")
           .transition()
           .duration(1000)
           .attr("width", function() { testing++; return xScale(updatedData[testing].option1); });
           // .attr("y", function(d) { return yScale(d.cases); })
 
+    //resets the lengths of each bar for option2 based on new data
     testing = -1;
     svgContainer.selectAll(".bar2")
           .transition()
@@ -365,9 +385,9 @@ var initialBar2 = function(data, l){
           .attr("width", function() { testing++; return xScale(updatedData[testing].option2); });
           // .attr("y", function(d) { return yScale(d.cases); })
 
+    //resets the bar number labels for option1 and transitions the previous numbers to the new data numbers
     testing = -1;
     var testing2 = -1;
-  //resets the number labels and transitioning the previous numbers to the new data numbers
     svgContainer.selectAll(".first")
         .transition()
         .duration(1000)
@@ -378,33 +398,33 @@ var initialBar2 = function(data, l){
           return function( t ) {
             this.textContent = interpolator( t );
           };})
-        .attr("x", function() { testing2++; return xScale(updatedData[testing2].option1) + 15; })
+        .attr("x", function() { testing2++; return xScale(updatedData[testing2].option1) + 12; })
         //.attr("x", (function(d) { return xScale(d.country) + xScale.bandwidth() / 2 ; }  ));
 
+    //resets the bar number labels for option2 and transitions the previous numbers to the new data numbers
     var label1 = -1;
     var label2 = -1;
-
     svgContainer.selectAll(".second")
         .transition()
         .duration(1000)
         .tween( 'text', function() {
           label1++;
-          // console.log(testing);
-          // console.log(updatedData[0].option2);
           var currentValue = this.textContent || "0";
           var interpolator = d3.interpolateRound( currentValue, updatedData[label1].option2);
           return function( t ) {
             this.textContent = interpolator( t );
           };})
-        .attr("x", function() { label2++; return xScale(updatedData[label2].option2) + 15; })
+        .attr("x", function() { label2++; return xScale(updatedData[label2].option2) + 12; })
   }
 
 }
 
 var updateChart = function() {
+    //getting new dataset based on slider value
     var data = getData(slider.value);
-    // console.log(data);
     var subData=[];
+
+    //checking chart type and cleaning up the dataset for passing to updateBar and updatePie functions
     if (chartType=="pie"){
       for (var i = 0; i < data.length; i++) {
           updatePie(data[i],i);
@@ -462,14 +482,12 @@ var updateChart = function() {
        dict.option1=data[i].recovered;
        subData.push(dict);
       }
-      // console.log("SUBDATA");
-      // console.log(subData);
       updateBar1(subData);
     }
 }
 
 var updatePie;
-// set the dimensions and margins of the graph
+
 var initialPie =function(dada){
   for (var i = 0; i < dada.length; i++) {
 var width = 450
@@ -555,14 +573,11 @@ var clearChart = function(){
 }
 
 var newGraph = function(){
-  // console.log("here");
-  // console.log(slider.value);
+  //getting new dataset based on slider value
   var data =  getData(slider.value);
   num = 0;
-  /*var data =  [{"location":"Canada", "cases":canadaN[num], "deaths":canadaD[num], "recovered":canadaR[num]},
-{"location":"China","cases":7,"deaths": 15, "recovered": 25},{"location":"France","cases":5,"deaths":20, "recovered": 8}]
-  console.log(data);*/
-  // console.log(data);
+
+  //checking for which options are checked
   var d = document.getElementById("d").checked;
   var r = document.getElementById("r").checked;
   var n =document.getElementById("n").checked;
@@ -570,10 +585,13 @@ var newGraph = function(){
   var options=[]
   clearChart();
   chartType="";
+
+  //cleaning up the dataset for passing to initialBar1 and initialBar2 and initialPie functions
   if(r && d && n ){
     chartType="pie";
     initialPie(data);
   }else if( d && r){
+    //setting chart type
     chartType="bar2dr";
     for (var i = 0; i < data.length; i++) {
      var dict={}
@@ -581,10 +599,12 @@ var newGraph = function(){
      dict.option1=data[i].deaths;
      dict.option2=data[i].recovered;
      subData.push(dict);
+     //setting label options
      options = ["Deaths", "Recoveries"];
     }
     initialBar2(subData, options);
   }else if( r && n){
+    //setting chart type
     chartType="bar2rn";
     for (var i = 0; i < data.length; i++) {
      var dict={}
@@ -592,10 +612,12 @@ var newGraph = function(){
      dict.option1=data[i].cases;
      dict.option2=data[i].recovered;
      subData.push(dict);
+     //setting label options
      options = ["Recoveries", "New Cases"];
     }
     initialBar2(subData, options);
   }else if( d && n){
+    //setting chart type
     chartType="bar2dn";
     for (var i = 0; i < data.length; i++) {
      var dict={}
@@ -603,41 +625,50 @@ var newGraph = function(){
      dict.option1=data[i].deaths;
      dict.option2=data[i].cases;
      subData.push(dict);
+     //setting label options
      options = ["Deaths", "New Cases"];
     }
     initialBar2(subData, options);
   }else if( r || d || n){
     if(r){
+      //setting chart type
       chartType="bar1r";
       for (var i = 0; i < data.length; i++) {
        var dict={}
        dict.country=data[i].location;
        dict.option1=data[i].recovered;
        subData.push(dict);
+       //setting label options
        options = ["Recoveries"];
       }
     }else if(d){
+      //setting chart type
       chartType="bar1d";
       for (var i = 0; i < data.length; i++) {
        var dict={}
        dict.country=data[i].location;
        dict.option1=data[i].deaths;
        subData.push(dict);
+       //setting label options
        options = ["Deaths"];
       }
     }else{
+      //setting chart type
+      chartType="bar1n";
       for (var i = 0; i < data.length; i++) {
-       chartType="bar1n";
        var dict={}
        dict.country=data[i].location;
        dict.option1=data[i].cases;
        subData.push(dict);
+       //setting label options
        options = ["New Cases"]
       }
     }
-    // console.log(subData);
+    //calling function to make single-bar bar graph
     initialBar1(subData, options);
   }
+
+  //displays transition and stop buttons only when options are chosen
   var transition = document.getElementById('update');
   if(r || d || n) {
     transition.style.display = "inline";
